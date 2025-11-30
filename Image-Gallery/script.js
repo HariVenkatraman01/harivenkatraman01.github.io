64632773f56ac932a3f9caec2ac3efd78250773d
HariVenkatraman01 <hariragavan.venkatraman@dcmail.ca>
2025-11-30 17:43:03 -0500

    Darken/Lighten Button

--- Image-Gallery/script.js
+++ Image-Gallery/script.js
@@ -36,3 +36,15 @@ function updateDisplayedImage(e) {
   displayedImage.src = e.target.src;
   displayedImage.alt = e.target.alt;
 }
+
+btn.addEventListener('click', () => {
+  if (btn.classList.contains('dark')) {
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = 'rgb(0 0 0 / 0.5)';
+    btn.classList.remove('dark'); 
+  } else {
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgb(0 0 0 / 0)';
+    btn.classList.add('dark');
+  }
+});
